5ed454b9dd6a5f4405ad55eb6269b86470e48971
Babatunde A Bello <Tundemusic10@gmail.com>
2025-04-30 15:37:01 -1100

    feat:an assignment

---
+++ app.js
@@ -0,0 +1,22 @@
+// Variables
+let number = 72;
+
+const position = "Quarterback";
+const loveNfl = true;
+let name;
+const myChest = null;
+const symbolValue = "id";
+const bigIntValue = 99999999999999999999999n;
+
+const classArray = [
+  72,
+  "Quarterback",
+  true,
+  null,
+  undefined,
+  { name: "Tunde" },
+  [1, 2, 3],
+  bigIntValue,
+  symbolValue,
+];
+console.log(classArray[4]);
