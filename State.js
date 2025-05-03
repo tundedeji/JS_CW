2f063701d841cdd85d2daba75f4ff62692ae891c
Adebanjo Olayinka <oadebanjo75@gmail.com>
2025-05-03 07:33:01 +0100

    Add State.js JavaScript file

---
+++ State.js
@@ -0,0 +1,9 @@
+//3 different statement within 1 curly braces
+{
+  let agencyName = "SubNova";
+  console.log("welcome the founder," + agencyName);
+  let yearFounded = 2025;
+  console.log(yearFounded);
+}
+
+
