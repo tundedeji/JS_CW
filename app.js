35d52b3fccc895c5bf740994c02800579a364ba6
Thompson I Anthony <izuchukwu498@gmail.com>
2025-05-03 03:59:17 -0700

    feat : new commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//
+let age = 36;
+
+const dating = false;
+
+const hobby = ["football", "FIFA", ["events" ,"sight - seeing"]]
+
+const biodata = {
+    height: "174cm"
+    weight: 84
+    blood: "b+",
+    gen:"AA" ,
+};
+
+const juliousInfo =[fullName, age, biodata, hobby, dating, age];
+
+
+console.log(typeof fullName, typeof age, typeof biodata, typeof hobby, typeof dating);
